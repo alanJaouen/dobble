0beac2b74fffe2f7d764b0dd912f8a04e98fb745
alanjaouen <alanjaouen@03d9d29e-4ed9-4a61-9318-1b61f731e1cd>
2015-11-23 18:59:30 +0000

    Changement de l'arborescance du projet pour etre compatible avec les test unitaires (Junit, jacoco) part 2

---
+++ dobble/src/main/java/dobble/Constantes_defaut_projet.java
@@ -0,0 +1,15 @@
+package dobble;
+
+public interface Constantes_defaut_projet {
+	// Attributs par défaut du projet:
+	
+	// Pour la classe Mode:
+	// Nombre par défaut de symboles sur les cartes:
+	public final static int nbSymbole_defaut = 9;
+	public final static int nbSymboleMax_defaut = 15;
+	
+	// Temps de réaction par défaut de l'IA (unité [à définir]):
+	public final static int tempsIA_defaut = 5;
+	public final static int tempsIAMax_defaut = 50;
+	// ---------------------------------------------------------------
+}
