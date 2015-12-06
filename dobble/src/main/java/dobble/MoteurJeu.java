b4e98e8b7e2b6f3ecb75f4b3f327add78d3e4994
gautierkyllian <gautierkyllian@03d9d29e-4ed9-4a61-9318-1b61f731e1cd>
2015-12-06 17:13:29 +0000

    Correction erreurs MoteurJeu

--- dobble/src/main/java/dobble/MoteurJeu.java
+++ dobble/src/main/java/dobble/MoteurJeu.java
@@ -127,7 +127,7 @@ public class MoteurJeu implements Serializable {
     public void interagir(int idJoueur,Symbole s) throws Exception
     {
     	// Si symbole commun est trouvé :
-    	if (s.equals(this.cartesCentre.get(this.cartesCentre.size() - 1).getSymboleCommun(this.arrayJoueur.get(idJoueur).getArrayCartes().get(this.arrayJoueur.get(idJoueur).getArrayCartes().size() - 1),
+    	if (s.equals(Carte.getSymboleCommun(this.arrayJoueur.get(idJoueur).getArrayCartes().get(this.arrayJoueur.get(idJoueur).getArrayCartes().size() - 1),
     			this.cartesCentre.get(cartesCentre.size() - 1)))) {
     		this.cartesCentre.remove(cartesCentre.size() - 1);
     		this.cartesCentre.add(this.arrayJoueur.get(idJoueur).getArrayCartes().get(this.arrayJoueur.get(idJoueur).getArrayCartes().size() - 1));
