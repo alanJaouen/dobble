cc0136605f0946711cce413f485f7697bd4ea6d1
seregris <seregris@03d9d29e-4ed9-4a61-9318-1b61f731e1cd>
2015-12-06 18:38:40 +0000

    Carte, MoteurJeu, TestMoteurJeu (en cours)

--- src/test/java/dobble/TestMoteurJeu.java
+++ src/test/java/dobble/TestMoteurJeu.java
@@ -1,16 +1,40 @@
 package dobble;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TestMoteurJeu {
 
 	public static void main(String[] args) {
-		ArrayList<Carte> carte = Carte.genererDeck(null);
-		ArrayList<Carte> arrayCartes, Stats stats, String nom, int score, String mdp
-		Joueur j1 = new Joueur();
-		ArrayList<Joueur> joueurs = new ArrayList<Joueur>();
-		joueurs.add(e)
-		MoteurJeu jeu = new MoteurJeu(ArrayList<Joueur> arrayJoueur, ArrayList<Carte> cartesCentre, Chronometer chrono, Mode modeDeJeu);
+		Mode mode = new Mode(); //Mode par defaut
+		ArrayList<Carte> deck = Carte.genererDeck(mode); //Nouveau deck global de 55 cartes
+		
+		Joueur j1 = new Joueur(new ArrayList<Carte>(), new Stats(), "Bonneau", 0, "mdp"); //Nouveau Joueur, sans deck pour l'instant
+		
+		ArrayList<Joueur> joueurs = new ArrayList<Joueur>(); //Liste de joueurs
+		joueurs.add(j1); //Ajout de notre joueur cree a la liste
+		
+		ArrayList<Carte> cartesCentre = new ArrayList<Carte>(); //Deck central
+		cartesCentre.add(new Carte(3)); //Première carte centrale, au pif
+		
+		
+		
+		MoteurJeu jeu = new MoteurJeu(joueurs); //Cree un moteur de jeu
+		
+		jeu.initialiser(); //initialisation: scores et chronometre
+		
+		jeu.lancerJeu(deck); //Lancement: cartes distribuees
+		
+		System.out.println("DEBUT JEU\n\n");
+		
+		
+		Scanner sc = new Scanner(System.in);
+		while (jeu.isInGame()) //BOUCLE DE JEU
+		{
+			System.out.println("DEBUT TOUR");
+			jeu.interagir(1, sc.nextInt());
+			
+		}
 	}
 
 }
