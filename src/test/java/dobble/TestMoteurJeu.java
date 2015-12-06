ddfc409817a60f4319d1ab54d6a7b82d47bad6c4
seregris <seregris@03d9d29e-4ed9-4a61-9318-1b61f731e1cd>
2015-12-06 18:06:05 +0000

    Main du moteur jeu (vide)

---
+++ src/test/java/dobble/TestMoteurJeu.java
@@ -0,0 +1,16 @@
+package dobble;
+
+import java.util.ArrayList;
+
+public class TestMoteurJeu {
+
+	public static void main(String[] args) {
+		ArrayList<Carte> carte = Carte.genererDeck(null);
+		ArrayList<Carte> arrayCartes, Stats stats, String nom, int score, String mdp
+		Joueur j1 = new Joueur();
+		ArrayList<Joueur> joueurs = new ArrayList<Joueur>();
+		joueurs.add(e)
+		MoteurJeu jeu = new MoteurJeu(ArrayList<Joueur> arrayJoueur, ArrayList<Carte> cartesCentre, Chronometer chrono, Mode modeDeJeu);
+	}
+
+}
