257e26a8e18aee8a9de3390d5ca285245f6cebdf
jupih <jupih@03d9d29e-4ed9-4a61-9318-1b61f731e1cd>
2015-12-18 11:35:43 +0000

    FentreJeu structuré

--- src/main/java/ihm/FenetreJeu.java
+++ src/main/java/ihm/FenetreJeu.java
@@ -1,10 +1,59 @@
 package ihm;
 
-public class FenetreJeu {
+import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
 
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import dobble.MoteurJeu;
+
+public class FenetreJeu extends JFrame{
+	
+
+	private MoteurJeu mj;
+	private JMenuBar menubar;
+	private JMenu menuQuitter;
+	
+	public FenetreJeu()
+	{
+		this.setTitle("Dobble");
+		Toolkit t =Toolkit.getDefaultToolkit();
+		Dimension d = t.getScreenSize();
+		this.setSize(d);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		menubar = this.creeMenuBar();
+		this.setJMenuBar(menubar);
+		
+		this.setVisible(true);
+		
+	}
+	
+	private JMenuBar creeMenuBar()
+	{
+		JMenuBar mb = new JMenuBar();
+		menuQuitter = creeMenuQuitter();
+		mb.add(menuQuitter);
+		return mb;
+	}
+	
+	private JMenu creeMenuQuitter()
+	{
+		JMenu menu = new JMenu();
+		JMenuItem quitter = new JMenuItem("quitter");
+		menu.add(quitter);
+		return menu;
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		FenetreJeu j= new FenetreJeu();
 	}
 
 }
