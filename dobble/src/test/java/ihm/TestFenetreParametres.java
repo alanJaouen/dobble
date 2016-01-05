c9f8b98a345ae309da8fe275f843a1ccb1bec7b7
gautierkyllian <gautierkyllian@03d9d29e-4ed9-4a61-9318-1b61f731e1cd>
2016-01-05 17:25:10 +0000

    Mise à jour de la fenetre des parametres:  - Ajout des éléments de l'interface  - Mise à jour des actions des boutons du bas
    ATTENTION : Fenetre ne fonctionne pas encore car non terminée.

---
+++ dobble/src/test/java/ihm/TestFenetreParametres.java
@@ -0,0 +1,12 @@
+package ihm;
+
+import dobble.Mode;
+
+public class TestFenetreParametres {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new FenetreParametres(new Mode());
+	}
+
+}
